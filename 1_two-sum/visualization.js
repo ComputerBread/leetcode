1525024ce64d2477e5d217f9adc7acfd338aca24
ComputerBread <contact.computerbread@gmail.com>
2024-09-25 04:58:41 +0200

    vis

--- 1_two-sum/visualization.js
+++ 1_two-sum/visualization.js
@@ -1,22 +1,41 @@
 const N = 10;
-const isBruteForce2 = true;
+const isBruteForce2 = false;
 
 function draw(I, J) {
 
   console.clear();
   console.log("")
 
+  let li1 = "        ";
+  let li2 = "        ";
+  for (let i = 0; i < N; i++) {
+    if (i == I) {
+      li1 += `   i    `;
+      li2 += `   ↓    `;
+    } else {
+      li1 += `        `;
+      li2 += `        `;
+    }
+  }
+  console.log(li1);
+  console.log(li2);
+  console.log("nums = [   0   ,   1   ,   2   ,   3   ,   4   ,   5   ,   6   ,   7   ,   8   ,   9   ]");
+
+  let l0 = "        ";
   let l1 = "        ";
   let l2 = "        ";
   for (let j = 0; j < N; j++) {
     if (j == J) {
+      l0 += `   ↑    `;
       l1 += `   j    `;
       l2 += `   ↓    `;
     } else {
+      l0 += `        `;
       l1 += `        `;
       l2 += `        `;
     }
   }
+  console.log(l0)
   console.log(l1)
   console.log(l2)
 
